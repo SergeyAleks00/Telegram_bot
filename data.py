5bff4df7bbd71b7f709a7b30a143d207284ad5e3
SegreyAleks00 <sergey.alekseyev23@mail.ru>
2024-10-01 01:23:44 +0300

    Trening version

---
+++ data.py
@@ -0,0 +1,54 @@
+import json
+DICT_DATA = 'data/quiz_data.json'
+quiz_data = [
+    {
+        'question': 'Что такое Python?',
+        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'options': ['float', 'int', 'str', 'natural'],
+        'correct_option': 1
+    },
+      {
+        'question': 'Какой оператор используется для определения переменной в Python?',
+        'options': ['var', 'print', 'if', 'def'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Как получить текущий день недели в Python?',
+        'options': ['datatime.now().weekday', 'time.weekday', 'date.weekdate', 'calendat.weekday'],
+        'correct_option': 0
+    },  {
+        'question': 'Как называется функция для работы с исключениями в Python?',
+        'options': ['raise', 'catch', 'except', 'handle'],
+        'correct_option': 3
+    },
+    {
+        'question': 'Что такое PEP в контексте Python?',
+        'options': ['Python Error Prevention', 'Python Extension Package', 'Python Enhancement Proposal', 'Python Execution Process'],
+        'correct_option': 2
+    },  {
+        'question': 'Как добавить новую строку в конец списка в Python?',
+        'options': ['list.append(element)', 'list.prepend(element)', 'list.insert(element)', 'list.extend(element)'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой из этих символов является комментарием в Python?',
+        'options': ['*', '/', '//', '#'],
+        'correct_option': 3
+    },  {
+        'question': 'Как обратиться к последнему элементу списка в Python?',
+        'options': ['mylist[-1]', 'mylistlength', 'mylist[:]', 'mylist.last()'],
+        'correct_option': 0
+    },
+    {
+        'question': 'В каком году был изозбретен Python',
+        'options': ['1996', '1969', '1979', '1967'],
+        'correct_option': 1
+    },
+]
+
+with open(DICT_DATA, 'w') as file:
+    json.dump(quiz_data, file)
